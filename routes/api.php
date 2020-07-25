a01d5656b47f003acddce55127bf3ca13abd3a61
Gilberto Lopes <gilbertolif@dcc.ufrj.br>
2020-07-25 15:49:37 -0300

    + api das cruds e de relacionamentos

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rotas da CRUD de locador
+Route::post('createLandlord', 'LandlordController@createLandlord');
+Route::get('showLandlord/{id}', 'LandlordController@showLandlord');
+Route::get('listLandlords', 'LandlordController@listLandlords');
+Route::put('updateLandlord/{id}', 'LandlordController@updateLandlord');
+Route::delete('deleteLandlord{id}', 'LandlordController@deleteLandlord');
+
+// Rotas da CRUD de anúncios
+Route::post('createAd', 'AdController@createAd');
+Route::get('showAd/{id}', 'AdController@showAd');
+Route::get('listAds', 'AdController@listAds');
+Route::put('updateAd/{id}', 'AdController@updateAd');
+Route::delete('deleteAd{id}', 'AdController@deleteAd');
+
+// Rotas da CRUD de locatário
+Route::post('createTenant', 'TenantController@createTenant');
+Route::get('showTenant/{id}', 'TenantController@showTenant');
+Route::get('listTenants', 'TenantController@listTenants');
+Route::put('updateTenant/{id}', 'TenantController@updateTenant');
+Route::delete('deleteTenant{id}', 'TenantController@deleteTenant');
+
+// Rotas de relacionamento do locatário
+Route::put('chooseAd/{id}/{id}', 'TenantController@chooseAd');
+Route::put('unchooseAd/{id}', 'TenantController@chooseAd');
